a1c2b07c9b7c470c8095fe2609e8625c951e8b36
jackie <luhaiming86@163.com>
2019-09-30 15:46:34 +0800

    add all files

--- src/main/java/com/jackie/kafkademo/ProducerTest.java
+++ src/main/java/com/jackie/kafkademo/ProducerTest.java
@@ -1,15 +1,79 @@
 package com.jackie.kafkademo;
 
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.Future;
+
+
+//https://www.cnblogs.com/yy3b2007com/p/8688289.html
 public class ProducerTest {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        producer_test1(args);
+        producer_test2();
     }
 
-    private static void producer_test1(){
+    private static void producer_test2() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.178.0.111:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for(int i = 0; i < 10; i++)
+            producer.send(new ProducerRecord<String, String>("kafakatopic", Integer.toString(i), Integer.toString(i)));
+
+        producer.close();
     }
 
-    private static void producer_test2(){
+    private static void producer_test1(String[] args) {
+        String arg0 = args != null && args.length > 0 ? args[0] : "10";
+        long events = Long.parseLong(arg0);
+        Random rnd = new Random();
 
+        //kafka topic
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.3.128:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        for (long nEvents = 0; nEvents < events; nEvents++) {
+            long runtime = new Date().getTime();
+            String ip = "192.178.0." + rnd.nextInt(255);
+            String msg = runtime + ",www.example.com," + ip;
+
+            ProducerRecord<String, String> data = new ProducerRecord<String, String>("kafakatopic", ip, msg);
+
+
+            Future<RecordMetadata> send = producer.send(data,
+                    new Callback() {
+                        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                            if (e != null) {
+                                e.printStackTrace();
+                            } else {
+                                System.out.println("The offset of the record we just sent is:" + recordMetadata.offset());
+                            }
+                        }
+                    }
+            );
+        }
+
+        producer.close();
     }
 }
---
+++ src/main/java/com/jackie/kafkademo/SimplePartitioner2.java
@@ -0,0 +1,32 @@
+package com.jackie.kafkademo;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
+
+import java.util.List;
+import java.util.Map;
+
+public class SimplePartitioner2 implements Partitioner {
+
+    public int partition(String topic, Object key, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
+        int partition = 0;
+
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+        String stringKey = (String) key;
+        int offset = stringKey.lastIndexOf(".");
+        if (offset > 0) {
+            partition = Integer.parseInt(stringKey.substring(offset + 1)) % numPartitions;
+        }
+        return partition;
+    }
+
+    public void close() {
+
+    }
+
+    public void configure(Map<String, ?> map) {
+
+    }
+}
